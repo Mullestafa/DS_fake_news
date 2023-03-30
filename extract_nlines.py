3beb689bdfe7a8dbb92f183ef603eaee019c5db4
Mullestafa <mustafahekmat@hotmail.dk>
2023-03-30 18:57:26 +0200

    latest

--- extract_nlines.py
+++
@@ -1,8 +0,0 @@
-from pyarrow.parquet import ParquetFile
-import pyarrow as pa 
-
-pf = ParquetFile('fake_news_output.parquet') 
-first_n_rows = next(pf.iter_batches(batch_size = 1,)) 
-df = pa.Table.from_batches([first_n_rows]).to_pandas()
-
-df.info()
